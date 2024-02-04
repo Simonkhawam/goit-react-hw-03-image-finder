5cba924f606778d578c0c4b4f19b1aaff41e9d0a
Simon Khawam <khawam.simon@me.com>
2024-02-04 22:49:19 +0200

    commit change

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
-export default async function articles( value, page = 1 ) {
+export default async function articles(value, page = 1) {
   const baseURL = 'https://pixabay.com/api/';
-  const API_KEY = '40925294-1286bb755e1bdf5717fd8e824';
+  const API_KEY = '41049165-3fd635f9aea88275b2a4dde7b';
 
   return await fetch(
     `${baseURL}?q=${value}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
